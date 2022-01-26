4559c3e6dd2121f2432190ad6625dbdaffdcf2db
rotemshaked <rotemshaked8@gmail.com>
2022-01-26 21:03:57 +0200

    done

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
 const User = require("../api/models/users");
+const path = require("path");
 const userRouter = require("./routes/users");
 require("dotenv").config();
+
+const publicPath = path.join(__dirname, "./client/build");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static(publicPath));
 app.use("/users", userRouter);
 
 const uri = process.env.ATLAS_URI;
