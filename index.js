6480bddc740e6bb4dd9574ac15dc26e806120abc
rotemshaked <rotemshaked8@gmail.com>
2022-01-26 21:14:23 +0200

    finish

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
+require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
-const User = require("../api/models/users");
 const path = require("path");
 const userRouter = require("./routes/users");
-require("dotenv").config();
 
 const publicPath = path.join(__dirname, "./client/build");
 
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri);
+
 const app = express();
-app.use(express.json());
+
 app.use(cors());
+app.use(express.json());
 app.use(express.static(publicPath));
 app.use("/users", userRouter);
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri);
-
-// const publicPath = "../client/build";
-// app.use(express.static(), publicPath);
+app.use("*", (req, res) => {
+  res.send("this route is not defind");
+});
 
 app.listen(process.env.PORT || 8080, () =>
   console.log(`Server is up and running on port ${process.env.PORT || 8080}`)
